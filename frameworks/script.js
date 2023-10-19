60938254cdf8f2af9d2e397fef6815528a8cb674
troc321 <77680359+troc321@users.noreply.github.com>
2023-10-19 12:32:21 -0400

    example

--- frameworks/script.js
+++ frameworks/script.js
@@ -111,7 +111,7 @@ const splash = new mojs.Burst({
     count:    8,
     timeline: { delay: 100 },
     children: {
-      stroke:       'white',
+      stroke:       'black',
       fill:         'none',
       scale:        'rand(.1,2)',
       strokeWidth:  { 8: 0 },
