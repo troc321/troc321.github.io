a2383517547f6e21f095957f0313a28bebf5d9c2
troc321 <77680359+troc321@users.noreply.github.com>
2023-12-07 15:55:58 -0500

    logo, pin tab, and other top element animations

--- RVHelpingHands/script.js
+++ RVHelpingHands/script.js
@@ -1,5 +1,5 @@
 gsap.to("#movingRV", {
-    duration: 18,
+    duration: 16,
     motionPath: {
       path: "#road-path",
       align: "#road-path",
@@ -8,15 +8,48 @@ gsap.to("#movingRV", {
     },
     scale: 1,
     repeat: -1,
-    repeatDelay: 0,
     ease: "sine.inOut"
   });
 
   
-  gsap.set("#whole-pin", {y: -100});
-
+  gsap.set("#whole-pin", {y: -150});
   gsap.to("#whole-pin", {
-    duration: 2,
-    y: "0",
+    duration: 1.25,
+    y: 0,
+    delay: 2.5, 
     ease: "bounce.out"
-  })
+  });
+
+  gsap.set("#pin-tab", {x: 1000});
+  gsap.to("#pin-tab", {
+    duration: .9,
+    x: 0,
+    delay: 1.51,
+    ease: "power1.out"
+  });
+
+
+  gsap.set("#logo", {scale: 0, transformOrigin: "center"});
+  gsap.to("#logo", {
+    duration: 1.25,
+    scale: 1,
+    delay: 3.25,
+    ease: "power1.out"
+  });
+
+  gsap.set("#on-site-text", {scale: 0, transformOrigin: "center"});
+  gsap.to("#on-site-text", {
+    duration: 1.25,
+    scale: 1,
+    delay: 3.5,
+    ease: "power1.out"
+  });
+
+  gsap.set("#learn-more-button", {scale: 0, transformOrigin: "center"});
+  gsap.to("#learn-more-button", {
+    duration: 1.25,
+    scale: 1,
+    delay: 4,
+    ease: "power1.out"
+  });
+  
